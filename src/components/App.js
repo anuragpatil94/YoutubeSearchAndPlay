59f669f77278c78ad88298cea8fd710ba03bd6c0
Anurag Patil <aspanurag@gmail.com>
2019-04-06 23:30:32 -0400

    All Components Structure| Stuck at Youtube API Part

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,16 @@ import VideoDetail from "./VideoDetail";
 import VideoList from "./VideoList";
 
 class App extends React.Component {
+  state = {};
+
+  onSearchSubmit = term => {
+    console.log(term);
+  };
+
   render() {
     return (
-      <div>
-        App Component
-        <SearchBar />
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
         <VideoDetail />
         <VideoList />
       </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,33 @@
+import "../stylesheets/SearchBar.css";
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { searchText: "" };
+
+  onInputChange = event => {
+    this.setState({ searchText: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.searchText);
+  };
+
   render() {
-    return <div>SearchBar Component</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={this.state.searchText}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
